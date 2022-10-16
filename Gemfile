e1cb7d670068411e61b4363b98a8310573748068
Charly Palencia <charlypalencia@Charlys-MacBook-Pro.local>
2022-10-16 11:31:01 -0500

    Setup capistrano

--- Gemfile
+++ Gemfile
@@ -9,7 +9,8 @@ gem 'rails', '~> 7'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'rack-cors'
 # Use Puma as the app server
-gem "puma", ">= 6"
+# gem "puma", ">= 6"
+gem 'puma', '~> 4.3.9'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -51,6 +52,13 @@ group :development do
   gem 'binding_of_caller'
   gem 'pry-rails'
   gem 'annotate', '~> 3.2'
+
+  gem 'capistrano', '~> 3.17', '>= 3.17.1'
+  gem 'capistrano3-puma', require: false
+  gem 'capistrano-bundler', require: false
+  gem 'capistrano-rbenv', require: false
+  gem 'capistrano-rbenv-install', '~> 1.2.0'
+  gem 'capistrano-rails', '~> 1.6', '>= 1.6.2'
 end
 
 group :test do
---
+++ config/deploy.rb
@@ -0,0 +1,88 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.17.1"
+
+set :application, "springBoard"
+set :repo_url, "git@github.com:Guarapo/SpringBoard.git"
+
+set :rbenv_ruby, '3.1.1'
+set :rbenv_path, "/home/deploy/.rbenv"
+
+set :user, "deploy"
+# set :default_env, { path: '~/.rbenv/shims:/usr/bin/:$PATH' }
+#
+# Default branch is :master
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+# Don't change these unless you know what you're doing
+set :pty,             false
+set :use_sudo,        true
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w[~/.ssh/id_rsa.pub] }
+
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true # Change to false when not using ActiveRecord
+
+append :rbenv_map_bins, 'puma', 'pumactl'
+
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", 'config/master.key'
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before 'deploy:starting', 'puma:make_dirs'
+end
+before 'deploy:assets:precompile', 'deploy:npm_install'
+namespace :deploy do
+  desc 'Run rake npm:install'
+  task :npm_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && npm install")
+        execute("cd #{release_path} && npm run postinstall")
+      end
+    end
+  end
+end
